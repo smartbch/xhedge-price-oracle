127af8166ac248517b8010399040f7cfb9d15c4d
zxh0 <zhangxiuhong2013@gmail.com>
2022-05-04 20:33:35 +0800

    Update update-oracle.js

--- scripts/update-oracle.js
+++ scripts/update-oracle.js
@@ -6,7 +6,7 @@ const { decrypt } = require('./mycrypto');
 const WINDOW_SIZE = 12 * 3600;
 const PERIOD_SIZE = 30 * 60;
 
-const priceOracleAddr = '0xa1b0C0158b8602de44A291B563FAd733baE10Eeb'; // TODO
+const priceOracleAddr = '0x45e23074777Dc816F5B26fd9eCF6d69c0728dd63';
 const priceOracleABI = [
   `function update() public`,
   `function getLastUpdatedTime() public view returns (uint64)`,
@@ -23,17 +23,20 @@ main()
 /*
   PRIV_KEY_FILE=pato/to/priv_key_file \
   SBCH_RPC_URL=http://13.214.157.111:8545 \
-  node update-oracle.js
+  node scripts/update-oracle.js
 */
 async function main() {
   const signer = await getSigner();
   const oracle = new ethers.Contract(priceOracleAddr, priceOracleABI, signer);
 
+  const delay = Number(process.env.DELAY_SECONDS || 0);
+  console.log('delay:', delay);
+
   while (true) {
     console.log('tick:', new Date());
 
     try {
-      await tryToUpdateOracle(oracle);
+      await tryToUpdateOracle(oracle, delay);
     } catch (err) {
       console.log('failed to update oracle:', err);
     }
@@ -56,30 +59,46 @@ async function getSigner() {
     [signer] = await ethers.getSigners();
     provider = signer.provider;
   }
+  console.log('adddr:', signer.address);
   return signer;
 }
 
-async function tryToUpdateOracle(oracle) {
-  const provider = oracle.signer.provider
-  const height = await provider.getBlockNumber()
-  const blk = await provider.getBlock(height)
-  const now = blk.timestamp
-  const price = await oracle.getPrice();
+async function tryToUpdateOracle(oracle, delay) {
+  const provider = oracle.signer.provider;
+  const now = await provider.getBlock("latest").then(blk => blk.timestamp);
+  const price = await oracle.getPrice().catch(err => 0);
   const lastUpdatedTime = await oracle.getLastUpdatedTime();
   console.log('now:', new Date(now * 1000));
   console.log('price:', ethers.utils.formatUnits(price));
   console.log('lastUpdatedTime:', new Date(lastUpdatedTime * 1000));
 
   if (now - lastUpdatedTime < PERIOD_SIZE) {
-    console.logg('oracle is up-to-date');
-  } else {
-    console.log('updating oracle ...');
-    const tx = await oracle.update();
-    console.log('tx:', tx);
-
-    const receipt = await tx.wait();
-    console.log('receipt:', receipt);
+    console.log('oracle is up-to-date');
+    return;
   }
+
+  if (delay > 0) {
+    console.log(`sleep ${delay} seconds...`);
+    await sleep(delay * 1000);
+
+    const now = await provider.getBlock("latest").then(blk => blk.timestamp);
+    const price = await oracle.getPrice().catch(err => 0);
+    const lastUpdatedTime = await oracle.getLastUpdatedTime();
+    console.log('now:', new Date(now * 1000));
+    console.log('price:', ethers.utils.formatUnits(price));
+    console.log('lastUpdatedTime:', new Date(lastUpdatedTime * 1000));
+    if (now - lastUpdatedTime < PERIOD_SIZE) {
+      console.log('oracle is up-to-date');
+      return;
+    }
+  }
+
+  console.log('updating oracle ...');
+  const tx = await oracle.update();
+  console.log('tx:', tx);
+
+  const receipt = await tx.wait();
+  console.log('receipt:', receipt);
 }
 
 function sleep(ms) {
